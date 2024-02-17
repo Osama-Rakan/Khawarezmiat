5b7bc90d5a51f94b09b48c451724b27794d8b92a
Osama Rakan Al Mraikhat <78223597+Osama-Rakan@users.noreply.github.com>
2024-02-17 15:12:41 +0300

    Add files via upload

---
+++ كود الفيديوهات/القائمة المرتبطة - Linked List/206. Reverse Linked List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        c = head
+        p = None
+        n = head.next
+        while c:
+            c.next = p
+            p = c
+            c = n
+            if n:
+                n = n.next
+        return p
